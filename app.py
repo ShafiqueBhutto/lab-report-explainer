712eebc4db2cd4594ba30fdc8c9a6fcab6f16c4e
unknown <shafiquebhutto304@gmail.com>
2025-08-23 12:50:03 +0500

    function to check value is within normal range

--- app.py
+++ app.py
@@ -5,14 +5,31 @@ st.title("Lab Report Explainer Dashboard")
 
 st.write("Upload the Lab report (CSV)/Excel to get started. ")
 
-uploaded_file = st.file_uploader("Upload Lab report", type=["csv", "xlsx"])
+def check_status(value, normal_range):
+    try:
+        if "-" in str(normal_range):
+            low, high = map(float, normal_range.split("-"))
+            value = float(value)
+            if value < low:
+                return "Low"
+            elif value > high:
+                return "High"
+            else:
+                return "Normal"
+            
+        elif "<" in str(normal_range):
+            limit = float(normal_range.replace("<", ''))
+            value = float(value)
+            return "High" if value >= limit else "Normal"
+        elif ">" in str(normal_range):
+            limit = float(normal_range.replace(">", ""))
+            value = float(value)
+            return "Low" if value <= limit else "Normal"
+        else:
+            return "Normal" if str(value) == str(normal_range) else "Abnormal"
+        
+    except: 
+        return "Unknown"
+    
 
-if uploaded_file is not None:
-    if uploaded_file.name.endswith(".csv"):
-        df = pd.read_csv(uploaded_file)
-    else:
-        df = pd.read_excel(uploaded_file)
-
-    st.subheader("Uploaded Report")
-    st.dataframe(df)
 
